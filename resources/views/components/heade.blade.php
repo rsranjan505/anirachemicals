7c978f1e543177158ff983798b90de7baa2483a6
rsranjan505 <rsranjan505@gmail.com>
2024-10-30 20:58:42 +0530

    menu 2

--- resources/views/components/heade.blade.php
+++ resources/views/components/heade.blade.php
@@ -61,7 +61,7 @@
                         <li class="{{Request::is('about') ? 'active' : ''}}"><a href="{{ route('about') }}">About Us</a></li>
                         <li class="has-dropdown {{Request::is('product') ? 'active' : ''}}">
                             <a href="{{ route('product') }}">Products</a>
-                            <ul style="width: 300px;">
+                            <ul style="width: 300px;overflow: scroll; height: calc(100vh - 20px);">
                                 @if (isset($products))
                                     @foreach ($products as $product)
                                         <li><a href="{{route('product.details',['name' => $product->slug])}}">{{ ucfirst($product->name)}}</a></li>
